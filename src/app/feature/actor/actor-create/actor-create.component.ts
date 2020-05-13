14d10ee42a6250c8ccecacac553e0bb821933d79
Jeff Peters <petersjeff288@gmail.com>
2020-05-12 22:01:54 -0400

    finished up actor components

--- src/app/feature/actor/actor-create/actor-create.component.ts
+++ src/app/feature/actor/actor-create/actor-create.component.ts
@@ -1,17 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { Actor } from 'src/app/model/actor.class';
+import { ActorService } from 'src/app/service/actor.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-actor-create',
-  templateUrl: './actor-create.component.html',
+  templateUrl: '../actor-maint-shared/actor-maint.component.html',
   styleUrls: ['./actor-create.component.css']
 })
 export class ActorCreateComponent implements OnInit {
   title: string = "Actor-Create";
   submitBtnTitle: string ="Create";
   
-  constructor() { }
+  actor: Actor = new Actor();
+
+  constructor(private actorSvc: ActorService,
+              private router: Router) { }
 
   ngOnInit(): void {
+    // do nothing
   }
-
+  save(){
+    this.actorSvc.create(this.actor).subscribe(jr=>{
+      //if jr.errors is null save was successful
+      if(jr.errors==null){
+        //success
+        this.router.navigateByUrl("/actor/list");
+      }
+      else {
+        console.log("******Error creating new actor: ", this.actor, jr.errors);
+      }
+    });
+  }
 }
